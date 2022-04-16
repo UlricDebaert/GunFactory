2e95a935e4e63bc267a5e0bc2ecf8a7f23c87b4b
Ulric <ulric.debaert@outlook.fr>
2022-04-16 09:52:01 +0200

    BaseController + Graphics

--- GunFactory/Assets/Scripts/PlayerController.cs
+++ GunFactory/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public float speed;
-
     public Rigidbody2D rb;
+
+    [Header("Ground Check")]
+    public Transform groundCheckPos;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    bool isGrounded;
+
+    [Header("Graphics")]
+    public SpriteRenderer playerSprite;
+
+    float horizontalInput;
 
     void Start()
     {
-        
+
     }
 
 
     void Update()
     {
         CheckInput();
+        CheckSurroundings();
     }
 
 
@@ -24,7 +36,33 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetButton("Horizontal"))
         {
-            rb.velocity = new Vector2(speed,rb.velocity.y);
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+
+            rb.velocity = new Vector2(speed * horizontalInput, rb.velocity.y);
         }
+
+        if(!Input.GetButton("Horizontal") && isGrounded)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (horizontalInput>0)
+        {
+            playerSprite.flipX = false;
+        }
+        if (horizontalInput<0)
+        {
+            playerSprite.flipX = true;
+        }
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheckPos.position, groundCheckRadius);
     }
 }
